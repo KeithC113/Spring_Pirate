a32041a1a844cc2d4e53ac6b6a1a90223bad7b63
KeithC113 <campbell_ka@yahoo.co.uk>
2020-07-27 20:16:49 +0100

    all tests passing, Pirate, Ship & Raids been added

---
+++ src/test/java/com/codeclan/example/pirateservice_d1_starter/PirateserviceD1StarterApplicationTests.java
@@ -0,0 +1,57 @@
+package com.codeclan.example.pirateservice_d1_starter;
+
+import com.codeclan.example.pirateservice_d1_starter.models.Pirate;
+import com.codeclan.example.pirateservice_d1_starter.models.Raid;
+import com.codeclan.example.pirateservice_d1_starter.models.Ship;
+import com.codeclan.example.pirateservice_d1_starter.repositories.PirateRepository;
+import com.codeclan.example.pirateservice_d1_starter.repositories.RaidRepository;
+import com.codeclan.example.pirateservice_d1_starter.repositories.ShipRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirateserviceD1StarterApplicationTests {
+
+	@Autowired
+	PirateRepository pirateRepository;
+
+	@Autowired
+	ShipRepository shipRepository;
+
+	@Autowired
+	RaidRepository raidRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void createPirateAndShip(){
+		Ship ship = new Ship("The Flying Dutchman");
+		shipRepository.save(ship);
+
+		Pirate pirate1 = new Pirate("Captain", "Pugwash", 56, ship);
+		pirateRepository.save(pirate1);
+
+	}
+	@Test
+	public void addPiratesAndRaids(){
+		Ship ship = new Ship ("Queen Anne's Revenge");
+		shipRepository.save(ship);
+
+		Pirate pirate = new Pirate ("Edward", "Teach", 45, ship);
+		pirateRepository.save(pirate);
+
+		Raid raid = new Raid ("Caribbean", 100);
+		raidRepository.save(raid);
+
+		raid.addPirate(pirate);
+		raidRepository.save(raid);
+
+	}
+
+}
